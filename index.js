3c1fa5fe4c085a8de8a7f5e1631eb2e58195621b
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-17 01:40:53 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  
+  if (feet <= 400){
+    return "This one is on me!";
+    
+  }
+  
+  // else if (feet >  400 && feet > 2000){
+  //   return 'I will gladly take your thirty bucks.';
+  // }
+  else if(feet > 2500){
+    return 'No can do.'
+  }
+  else if(feet > 2000){
+    return "I will gladly take your thirty bucks."
+  }
+  
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  // let city;
+  // const checkCity = city === "NYC" ? "Ok, sounds good." : "No go."
+  // checkCity;
+
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous" :
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+    default:
+      return "Bye."
+      
+  }
 }
